b3ec7d61e405a336b6c6f6bcda6ea5c2fecabe92
zhaqsybayuly <zhaqsybayuly@gmail.com>
2026-01-29 00:15:28 +0500

    Add syntax example

---
+++ Practice01/syntax.py
@@ -0,0 +1,19 @@
+# Task 1
+print("Hello, World!")
+
+# Task 2
+if 5 > 2:
+    print("Five is greater than two!")
+
+# Task 3
+print("Statement 1"); print("Statement 2")
+
+# Task 4
+x = 5
+y = "Hello"
+print(x)
+print(y)
+
+# Task 5
+if 10 > 5:
+    print("Ten is greater than five")
