0ecabb8332735b8bbb6b364a22a40db2150e4cb3
zhaqsybayuly <zhaqsybayuly@gmail.com>
2026-01-29 00:14:26 +0500

    Add comments example

---
+++ Practice01/comments.py
@@ -0,0 +1,22 @@
+# Task 1
+# This is a comment
+print("Hello, World!")
+
+# Task 2
+print("Hello, World!") # This is a comment
+
+# Task 3
+# print("This code is commented out")
+print("This code will run")
+
+# Task 4
+# This is a
+# multi-line comment
+print("Multi-line example")
+
+# Task 5
+"""
+This is also a
+multi-line comment
+"""
+print("Docstring example")
