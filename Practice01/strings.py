a53755d166a8098e59df885bf78ae7e666c11253
zhaqsybayuly <zhaqsybayuly@gmail.com>
2026-01-29 00:15:24 +0500

    Add strings example

---
+++ Practice01/strings.py
@@ -0,0 +1,22 @@
+# Task 1
+a = """Lorem ipsum dolor sit amet,
+consectetur adipiscing elit."""
+print(a)
+
+# Task 2
+b = "Hello, World!"
+print(b[2:5])
+
+# Task 3
+a = "Hello, World!"
+print(a.upper())
+print(a.lower())
+
+# Task 4
+a = "Hello, World!"
+print(a.replace("H", "J"))
+
+# Task 5
+age = 36
+txt = "My name is John, and I am {}"
+print(txt.format(age))
