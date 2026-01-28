6a414ad47e0fda41f7ddfee7c175877becd16c82
zhaqsybayuly <zhaqsybayuly@gmail.com>
2026-01-29 00:15:13 +0500

    Add data types example

---
+++ Practice01/data_types.py
@@ -0,0 +1,23 @@
+# Task 1
+x = "Hello World"
+print(type(x))
+
+# Task 2
+x = 20
+y = 20.5
+z = 1j
+print(type(x), type(y), type(z))
+
+# Task 3
+x = ["apple", "banana", "cherry"]
+y = ("apple", "banana", "cherry")
+z = range(6)
+print(type(x), type(y), type(z))
+
+# Task 4
+x = {"name" : "John", "age" : 36}
+print(type(x))
+
+# Task 5
+x = True
+print(type(x))
