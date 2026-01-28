f665ce01ae62511bf65aa27a75f6531511347212
zhaqsybayuly <zhaqsybayuly@gmail.com>
2026-01-29 00:15:32 +0500

    Add variables example

---
+++ Practice01/variables.py
@@ -0,0 +1,27 @@
+# Task 1
+x = 5
+y = "John"
+print(x)
+print(y)
+
+# Task 2
+x = str(3)
+y = int(3)
+z = float(3)
+print(x, y, z)
+
+# Task 3
+x = 5
+y = "John"
+print(type(x))
+print(type(y))
+
+# Task 4
+x, y, z = "Orange", "Banana", "Cherry"
+print(x, y, z)
+
+# Task 5
+x = "Python "
+y = "is "
+z = "awesome"
+print(x + y + z)
